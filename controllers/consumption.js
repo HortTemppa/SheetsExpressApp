f505bb5d10f2d51d6d087299acb0fee0e871ee5c
HortTemppa <temelameni@gmail.com>
2020-03-21 21:53:38 +0200

    Basic structure done. GET request that returns row data done.

--- controllers/consumption.js
+++ controllers/consumption.js
@@ -1,11 +1,41 @@
-const consumptionRouter = require("express").Router();
-const bodyParser = require("body-parser");
+require("dotenv").config();
+const sheetsRouter = require("express").Router();
+const { GoogleSpreadsheet } = require("google-spreadsheet");
 
-consumptionRouter.post('/', async(request, response, next) => {
+const credentials = require("../client_secret.json");
 
+sheetsRouter.post("/", async (request, response, next) => {
+  return null;
+});
 
-})
+sheetsRouter.get("/", async (request, response) => {
+  const document = await new GoogleSpreadsheet(process.env.SPREADSHEET_ID);
 
-consumptionRouter.get('/', async(request, response, next) => {
+  await document.useServiceAccountAuth({
+    client_email: credentials.client_email,
+    private_key: credentials.private_key
+  });
 
-})
+  await document.loadInfo();
+
+  const sheet = await document.sheetsByIndex[0];
+  const rows = await sheet.getRows({
+    offset: 0
+  });
+
+  const consumables = [];
+
+  rows.forEach(row => {
+    consumables.push({
+      tuote: row.tuote,
+      yksikkö: row.yksikkö,
+      co2: row.co2,
+      määrä: row.määrä,
+      kategoria: row.kategoria
+    });
+  });
+
+  return response.json(consumables);
+});
+
+module.exports = sheetsRouter;
--- server.js
+++ server.js
@@ -1,12 +1,49 @@
 require("dotenv").config();
-const express = require('express')
-const app = express()
 
-const consumptionRouter = require('./controllers/consumption')
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const { promisify } = require("util");
+const express = require("express");
+const app = express();
 
-app.use('/api/consumption', consumptionRouter)
+const credentials = require("./client_secret.json");
 
-const PORT = process.env.PORT
+const sheetsRouter = require("./controllers/consumption");
+
+const accessSpreadsheet = async () => {
+  const document = await new GoogleSpreadsheet(
+    "18Ou6uQjD95lL26RCP8MM0LZRkT4aQfInsLZWRomNbaY"
+  );
+
+  await document.useServiceAccountAuth({
+    client_email: credentials.client_email,
+    private_key: credentials.private_key
+  });
+
+  await document.loadInfo();
+
+  const sheet = await document.sheetsByIndex[0];
+  const rows = await sheet.getRows({
+    offset: 0
+  });
+
+  const consumables = [];
+
+  rows.forEach(row => {
+    consumables.push({
+      tuote: row.tuote,
+      yksikkö: row.yksikkö,
+      co2: row.co2,
+      määrä: row.määrä
+    });
+  });
+  console.log(consumables);
+};
+
+accessSpreadsheet();
+
+app.use("/api/sheets/", sheetsRouter);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Serveri juoksee portissa ${PORT}`)
-})
+  console.log(`Serveri juoksee portissa ${PORT}`);
+});
