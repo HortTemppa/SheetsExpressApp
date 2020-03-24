95ad90ee3f87de24f4ce3d0ae2ecf029f58fb9f4
HortTemppa <temelameni@gmail.com>
2020-03-24 19:49:57 +0200

    PUT data into a cell working

--- controllers/consumption.js
+++ controllers/consumption.js
@@ -9,19 +9,42 @@ const credentials = require("../client_secret.json");
 sheetsRouter.use(bodyParser.json());
 sheetsRouter.use(bodyParser.urlencoded({ extended: true }));
 
-sheetsRouter.post("/", async (request, response, next) => {
+sheetsRouter.put("/", async (request, response) => {
   const document = await new GoogleSpreadsheet(process.env.SPREADSHEET_ID);
-
-  const data = request.body;
-
-  //
-
-  console.log(data);
 
   await document.useServiceAccountAuth({
     client_email: credentials.client_email,
     private_key: credentials.private_key
   });
+
+  await document.loadInfo();
+
+  const sheet = await document.sheetsByIndex[0];
+
+  await sheet.loadCells(request.body.cell);
+
+  const cell = await sheet.getCellByA1(request.body.cell);
+
+  const data = request.body.data;
+
+  cell.value = data;
+
+  await sheet.saveUpdatedCells();
+
+  return response.status(200).send();
+});
+
+sheetsRouter.post("/", async (request, response, next) => {
+  const document = await new GoogleSpreadsheet(process.env.SPREADSHEET_ID);
+
+  await document.useServiceAccountAuth({
+    client_email: credentials.client_email,
+    private_key: credentials.private_key
+  });
+
+  const data = request.body;
+
+  console.log(data);
 
   await document.loadInfo();
 
@@ -47,16 +70,11 @@ sheetsRouter.get("/", async (request, response) => {
 
   const sheetHeaders = sheet.headerValues;
 
-  console.log("rows", rows);
-  console.log("Headers length:" + sheetHeaders.length);
-
   const rowData = new Array(rows.length);
 
   for (var i = 0; i < rowData.length; i++) {
     rowData[i] = new Array(sheetHeaders.length);
   }
-
-  console.log("RowCount:" + sheet.rowCount);
 
   await rows.forEach((row, i) => {
     for (let j = 0; j < sheetHeaders.length; j++) {
